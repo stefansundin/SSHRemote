eb049c87b7c0be270104a1f1d2120c24890c7465
Stefan Sundin <git@stefansundin.com>
2025-12-07 12:16:45 -0800

    EditHostScreen: Add bottom spacer to account for FAB.

--- app/src/main/java/com/stefansundin/sshremote/ui/screens/EditHostScreen.kt
+++ app/src/main/java/com/stefansundin/sshremote/ui/screens/EditHostScreen.kt
@@ -21,9 +21,12 @@ package com.stefansundin.sshremote.ui.screens
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -486,6 +489,8 @@ fun EditHostScreen(
                     }
                 }
             }
+
+            Spacer(Modifier.height(80.dp))
         }
     }
 }
