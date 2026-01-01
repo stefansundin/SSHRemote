bffeb925f31124ec212547ea8dadc7c008d5b84a
Stefan Sundin <git@stefansundin.com>
2025-12-31 20:43:25 -0800

    Upgrade to Java 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,13 +39,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
     kotlin {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     buildFeatures {
