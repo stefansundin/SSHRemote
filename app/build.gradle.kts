2d80e90a420bfda3cb1875931143e579b2ebc1fe
Stefan Sundin <git@stefansundin.com>
2026-05-25 12:16:52 -0700

    Prepare for release 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.stefansundin.sshremote"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
         base.archivesName = "ssh-remote-$versionName"
 
